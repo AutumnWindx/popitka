d5747685cee9e7814d8572c3b02a88acdadbbaf5
Виктор <footbolist75@mail.ru>
2023-04-23 18:16:14 +0700

    This function takes in an array of integers and returns the maximum value in the array

---
+++ program.cs
@@ -0,0 +1,23 @@
+static void Sum(int num1, int num2)
+{
+  int sum = num1 + num2;
+  Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, sum);
+}
+
+static void DisplayString(string message)
+{
+  Console.WriteLine(message);
+}
+
+static int FindMax(int[] arr)
+{
+  int max = arr[0];
+  for (int i = 1; i < arr.Length; i++)
+  {
+    if (arr[i] > max)
+    {
+      max = arr[i];
+    }
+  }
+  return max;
+}
