8909cb3ff9c9098024db03b7826e148b498fdd0e
Виктор <footbolist75@mail.ru>
2023-04-23 18:11:49 +0700

    This function takes in a string input and displays it on the console

---
+++ program.cs
@@ -0,0 +1,10 @@
+static void Sum(int num1, int num2)
+{
+  int sum = num1 + num2;
+  Console.WriteLine("The sum of {0} and {1} is {2}", num1, num2, sum);
+}
+
+static void DisplayString(string message)
+{
+  Console.WriteLine(message);
+}
